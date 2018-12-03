9c178355fefe1ec47f48ab1f292768b3ec465fec
Pritam Soni <pritamsoni5555@gmail.com>
2018-12-03 21:49:49 +0530

    changes done in all files

--- experts.php
+++ experts.php
@@ -2,18 +2,19 @@
 <html>
 
 <head>
-    <title>Global Essay, Dissertation and Assignment Help </title>
+    <title>OnlinAssignmenthelp.com - About Us</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta title="Looking for online essay, thesis or dissertation help? OnlineAssignmenthelp.com is your destination. We offer global assignment help service for all subjects at unbelievably cheap price.
+    <meta title="We are the leading assignment help provider in Australia. We have the best assignment experts on board who provide top notch essay, dissertation and assignment help. 
+
                 ">
     <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/custom.css">
     <link rel="stylesheet" type="text/css" href="./assets/css/font.css">
     <link rel="stylesheet" type="text/css" href="./assets/css/animated.css">
     <link rel="stylesheet" type="text/css" href="./assets/css/scrollanimations.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.css"
     />
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
 
