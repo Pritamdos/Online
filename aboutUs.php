f9ad6393c50014a3155e2cfbe27504c62876e943
Pritam Soni <pritamsoni5555@gmail.com>
2018-12-03 20:27:45 +0530

    changes done in all files

--- aboutUs.php
+++ aboutUs.php
@@ -2,11 +2,13 @@
 <html>
 
 <head>
-    <title>Global Essay, Dissertation and Assignment Help </title>
+    <title>OnlinAssignmenthelp.com - About Us</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta title="Looking for online essay, thesis or dissertation help? OnlineAssignmenthelp.com is your destination. We offer global assignment help service for all subjects at unbelievably cheap price.
+    <meta title="We are the leading assignment help provider in Australia. We have the best assignment experts on board who provide top notch essay, dissertation and assignment help. 
+
                 ">
     <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/custom.css">
     <link rel="stylesheet" type="text/css" href="./assets/css/font.css">
     <link rel="stylesheet" type="text/css" href="./assets/css/animated.css">
     <link rel="stylesheet" type="text/css" href="./assets/css/scrollanimations.css">
@@ -19,6 +21,300 @@
 
 <body>
     <?php include './header.php'?>
+    <div class="onl-banner-sections">
+        <div class="onl-inrs-banner-sections-abouts">
+            <div class="onl-sub-inners-sec">
+                <div class="onl-sub-inners-about-section animated fadeInLeft">
+                    <span class="welcome-online-help ">
+                    Want Your Essays, Dissertation, Thesis and Assignment To Be Written By The World’s Top Assignment Helper?
+
+
+                    </span>
+                </div>
+
+            </div>
+            <div class="notifiicons">
+                <div class="notif-flex">
+                    <div class="notif-btn">
+                        <span class="call-iconss swing">
+                            <i class="fa fa-phone calls" aria-hidden="true"></i>
+                        </span>
+
+                        <span class="call-nows">
+                            Call
+                            <br> Back
+                        </span>
+                        </span>
+                    </div>
+                </div>
+                <div class="notify-outer-popos">
+                    <div class="notif-item animated slideInUp slideInDown hide">
+                        <div class="notify-inners-sec">
+                            <div class="notify-haeders-sec">
+                                <span class="online-headers-test">
+                                    Welcome in Online Asssigment
+                                </span>
+                                <span class="closed-iconss" id="slide-down">
+
+                                    <i class="fa fa-times  drop-arrow" aria-hidden="true"></i>
+                                </span>
+                            </div>
+                            <div class="notify-form-outers">
+                                <div class="form-inner-sections">
+                                    <div class="each-rows-inputs">
+                                        <input type="text" placeholder="Enter your name" />
+                                        <span class="users-pp">
+                                            <i class="fa fa-user" aria-hidden="true"></i>
+
+                                        </span>
+                                    </div>
+                                    <div class="each-rows-inputs">
+                                        <input type="text" placeholder="Enter your phone" />
+                                        <span class="users-pp">
+                                            <i class="fa fa-mobile" aria-hidden="true"></i>
+
+                                        </span>
+
+                                    </div>
+                                    <div class="each-rows-inputs">
+                                        <input type="email" placeholder="Enter your email" />
+                                        <span class="users-pp">
+                                            <i class="fa fa-envelope" aria-hidden="true"></i>
+
+                                        </span>
+                                    </div>
+                                    <div class="each-rows-inputs each-rows-inputss">
+                                        <span class="deadline-text-zone">
+                                            Time Zone
+                                        </span>
+                                        <div class="selected-timezone">
+
+
+                                            <select class="setected-time-zone">
+                                                <option>1</option>
+                                                <option>2</option>
+                                                <option>3</option>
+                                                <option>4</option>
+                                                <option>5</option>
+                                            </select>
+                                            <span class="users-pp">
+                                                <i class="fa fa-clock-o" aria-hidden="true"></i>
+
+                                            </span>
+
+                                        </div>
+
+
+
+
+                                    </div>
+
+                                    <div class="each-rows-inputs each-rows-inputss">
+                                        <span class="deadline-text">
+                                            Expected Calling Date
+                                        </span>
+                                        <div class="each-rows-deadline-sec">
+                                            <div class="date-starts-left">
+                                                <input type="date" placeholder="Enter start date" />
+                                            </div>
+                                            <div class="date-starts-right">
+                                                <input type="date" placeholder="Enter last date" />
+                                            </div>
+                                        </div>
+
+
+                                    </div>
+                                    <div class="each-rows-inputs">
+                                        <span class="deadline-text-zone">
+                                            Preferable time
+                                        </span>
+                                        <div class="each-rows-deadline-time-zone">
+                                            <div class="date-starts-time">
+                                                <input type="time" placeholder="Enter start date" />
+                                                <span class="users-pp">
+                                                    <i class="fa fa-clock-o" aria-hidden="true"></i>
+
+                                                </span>
+                                            </div>
+
+                                        </div>
+
+
+                                    </div>
+                                </div>
+                                <div class="submit-buttin-sections">
+                                    <button type="submit" class="only-submit">
+                                        Submit
+
+                                    </button>
+
+                                </div>
+                            </div>
+
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="dash-cont">
+        <div class="wrapper">
+            <div class="header-sections">
+                <span class="world-abouts">
+                Get customized solutions from the best writers and score high
+
+
+                </span>
+            </div>
+            <div class="dashboard-inner-wraper">
+                <div class="card-outer-about animatable bounceInLeft">
+                  <span class="about-assigments">
+                  OnlineAssignmenthelp.com has been one of the top names in the field of assignment writing services for a long time now. We are a dedicated lot whose sole purpose is to help you achieve academic excellence in all subjects and at all educational levels. We treat delivering high quality assignments as top priority because your grades matter as much to us as they do to you. 
+                  </span>
+
+                </div>
+               
+
+
+            </div>
+
+
+        </div>
+    </div>
+    <div class="dash-cont how-work">
+        <div class="wrapper">
+            <div class="header-sections">
+                <span class="world-abouts">
+                 Our Vision
+
+
+                </span>
+            </div>
+            <div class="dashboard-inner-wraper">
+                <div class="card-outer-about animatable bounceInLeft">
+                  <span class="about-assigments">
+                  Our vision is to occupy the top spot in the whole world and be known as the world’s No. 1 assignment writing service provider. Our aim is not to be referred to as the largest assignment helper but to be recognized as one that provides incomparable quality of services, be it in terms of writing, prices or delivery timing. We want to create a world where students get access to premium essay, dissertation, thesis & assignment writing services in all corners of the world. 
+
+                  </span>
+
+                </div>
+               
+
+
+            </div>
+
+
+        </div>
+    </div>
+    <div class="dash-cont">
+        <div class="wrapper">
+            <div class="header-sections">
+                <span class="world-abouts">
+                Our Mission 
+
+
+                </span>
+            </div>
+            <div class="dashboard-inner-wraper">
+                <div class="card-outer-about animatable bounceInLeft">
+                  <span class="about-assigments">
+                  Our mission is simple – to help students of all disciplines and educational levels achieve the highest grades and top marks in their respective fields. We want to empower students and help them secure a grand career for themselves so they can walk with their heads held high. 
+
+                  </span>
+
+                </div>
+               
+
+
+            </div>
+
+
+        </div>
+    </div>
+    <div class="dash-cont how-work h-online">
+        <div class="wrapper">
+            <div class="header-sections">
+                <span class="world-abouts-orders">
+
+                    <span class="orders-nows">
+                      Our Values 
+                    </span>
+                    <span class="core">
+                       Our core values are three-fold and help us in achieving our vision and mission- 
+                    </span>
+                </span>
+            </div>
+
+            <div class="dashboard-inner-wraper">
+                <div class="card-outer-work-hire fadeInLeft animated">
+                    <div class="card-inner-section-work work-heir">
+                        <div class="work-headers-works">
+                        To deliver work of the highest quality
+
+                        </div>
+                        <div class="card-online-list card-online-hr">
+                            <ul>
+                                <li>
+                                There will be no compromises in terms of quality from our end. As assignment helpers, our top priority is to deliver original solutions of supreme quality that impart knowledge. 
+
+                                </li>
+                              
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-outer-work-hire fadeInRight animated">
+                    <div class="card-inner-section-work  work-heir">
+                        <div class="work-headers-works">
+                           To always ensure on-time delivery
+
+
+                        </div>
+                        <div class="card-online-list card-online-hr">
+                            <ul>
+                                <li>
+                                  We vow to prioritize your assignments and never skip an important deadline. In fact, we deliver high quality assignments before deadline. 
+
+                                </li>
+                             
+
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="card-outer-work-hire fadeInRight animated">
+                    <div class="card-inner-section-work  work-heir">
+                        <div class="work-headers-works">
+                        To provide assignments at pocket-friendly prices: 
+
+
+                        </div>
+
+                        <div class="card-online-list card-online-hr">
+                            <ul>
+                                <li>
+                                We believe that nothing should hinder your academic progress, especially not money. This is why we offer our services at prices you can’t refuse. 
+                                </li>
+                               
+
+
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+
+
+            </div>
+
+
+        </div>
+    </div>
+
     <?php include './footer.php'?>
 
 
--- privacy.php
+++ privacy.php
@@ -2,11 +2,15 @@
 <html>
 
 <head>
-    <title>Global Essay, Dissertation and Assignment Help </title>
+    <title>Strict Privacy Policy of OnlineAssignmenthelp.com</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta title="Looking for online essay, thesis or dissertation help? OnlineAssignmenthelp.com is your destination. We offer global assignment help service for all subjects at unbelievably cheap price.
+    <meta title="
+    Transparency and honesty are the pillars our assignment help services rely on. Check out our privacy policy before availing essay, dissertation and thesis help from us. 
+
+
                 ">
     <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/custom.css">
     <link rel="stylesheet" type="text/css" href="./assets/css/font.css">
     <link rel="stylesheet" type="text/css" href="./assets/css/animated.css">
     <link rel="stylesheet" type="text/css" href="./assets/css/scrollanimations.css">
@@ -19,6 +23,146 @@
 
 <body>
     <?php include './header.php'?>
+    <div class="onl-banner-sections">
+        <div class="onl-inrs-banner-sections-privacy">
+            <div class="onl-sub-inners-sec">
+                <div class="onl-sub-inners-about-section animated fadeInLeft">
+                    <span class="welcome-online-help ">
+                    Transparency and honesty are the pillars our assignment help services rely on. Check out our privacy policy before availing essay, dissertation and thesis help from us. 
+
+
+
+                    </span>
+                </div>
+
+            </div>
+            <div class="notifiicons">
+                <div class="notif-flex">
+                    <div class="notif-btn">
+                        <span class="call-iconss swing">
+                            <i class="fa fa-phone calls" aria-hidden="true"></i>
+                        </span>
+
+                        <span class="call-nows">
+                            Call
+                            <br> Back
+                        </span>
+                        </span>
+                    </div>
+                </div>
+                <div class="notify-outer-popos">
+                    <div class="notif-item animated slideInUp slideInDown hide">
+                        <div class="notify-inners-sec">
+                            <div class="notify-haeders-sec">
+                                <span class="online-headers-test">
+                                    Welcome in Online Asssigment
+                                </span>
+                                <span class="closed-iconss" id="slide-down">
+
+                                    <i class="fa fa-times  drop-arrow" aria-hidden="true"></i>
+                                </span>
+                            </div>
+                            <div class="notify-form-outers">
+                                <div class="form-inner-sections">
+                                    <div class="each-rows-inputs">
+                                        <input type="text" placeholder="Enter your name" />
+                                        <span class="users-pp">
+                                            <i class="fa fa-user" aria-hidden="true"></i>
+
+                                        </span>
+                                    </div>
+                                    <div class="each-rows-inputs">
+                                        <input type="text" placeholder="Enter your phone" />
+                                        <span class="users-pp">
+                                            <i class="fa fa-mobile" aria-hidden="true"></i>
+
+                                        </span>
+
+                                    </div>
+                                    <div class="each-rows-inputs">
+                                        <input type="email" placeholder="Enter your email" />
+                                        <span class="users-pp">
+                                            <i class="fa fa-envelope" aria-hidden="true"></i>
+
+                                        </span>
+                                    </div>
+                                    <div class="each-rows-inputs each-rows-inputss">
+                                        <span class="deadline-text-zone">
+                                            Time Zone
+                                        </span>
+                                        <div class="selected-timezone">
+
+
+                                            <select class="setected-time-zone">
+                                                <option>1</option>
+                                                <option>2</option>
+                                                <option>3</option>
+                                                <option>4</option>
+                                                <option>5</option>
+                                            </select>
+                                            <span class="users-pp">
+                                                <i class="fa fa-clock-o" aria-hidden="true"></i>
+
+                                            </span>
+
+                                        </div>
+
+
+
+
+                                    </div>
+
+                                    <div class="each-rows-inputs each-rows-inputss">
+                                        <span class="deadline-text">
+                                            Expected Calling Date
+                                        </span>
+                                        <div class="each-rows-deadline-sec">
+                                            <div class="date-starts-left">
+                                                <input type="date" placeholder="Enter start date" />
+                                            </div>
+                                            <div class="date-starts-right">
+                                                <input type="date" placeholder="Enter last date" />
+                                            </div>
+                                        </div>
+
+
+                                    </div>
+                                    <div class="each-rows-inputs">
+                                        <span class="deadline-text-zone">
+                                            Preferable time
+                                        </span>
+                                        <div class="each-rows-deadline-time-zone">
+                                            <div class="date-starts-time">
+                                                <input type="time" placeholder="Enter start date" />
+                                                <span class="users-pp">
+                                                    <i class="fa fa-clock-o" aria-hidden="true"></i>
+
+                                                </span>
+                                            </div>
+
+                                        </div>
+
+
+                                    </div>
+                                </div>
+                                <div class="submit-buttin-sections">
+                                    <button type="submit" class="only-submit">
+                                        Submit
+
+                                    </button>
+
+                                </div>
+                            </div>
+
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
     <?php include './footer.php'?>
 
 
