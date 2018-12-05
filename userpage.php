0e25f58fae48a6b7bfa56e52f01003b5538af99a
sonipritam <pritam.kumar@venturepact.com>
2018-12-05 15:34:54 +0530

    changes done in bug

--- userpage.php
+++ userpage.php
@@ -7,12 +7,12 @@
     <meta title="Looking for online essay, thesis or dissertation help? OnlineAssignmenthelp.com is your destination. We offer global assignment help service for all subjects at unbelievably cheap price.
                 ">
     <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/custom.css">
     <link rel="stylesheet" type="text/css" href="./assets/css/font.css">
     <link rel="stylesheet" type="text/css" href="./assets/css/animated.css">
     <link rel="stylesheet" type="text/css" href="./assets/css/scrollanimations.css">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.css"
-    />
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://npmcdn.com/flickity@1.2/dist/flickity.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
@@ -21,8 +21,7 @@
     <?php include './header.php'?>
     <div class="onl-banner-sections">
         <div class="onl-inrs-banner-sections-login-signups">
-
-            <div class="notifiicons">
+        <div class="notifiicons">
                 <div class="notif-flex">
                     <div class="notif-btn">
                         <span class="call-iconss swing">
@@ -37,108 +36,154 @@
                     </div>
                 </div>
                 <div class="notify-outer-poposs">
-                    <div class="notif-item animated slideInUp slideInDown hide">
+                    <div class="notif-item notif-item-items animated slideInUp slideInDown hide">
                         <div class="notify-inners-sec">
-                            <div class="notify-haeders-sec">
-                                <span class="online-headers-test">
-                                    Welcome in Online Asssigment
-                                </span>
+                             <div class="cross-ss">
                                 <span class="closed-iconss" id="slide-down">
-
-                                    <i class="fa fa-times  drop-arrow" aria-hidden="true"></i>
+                                <i class="fa fa-times  drop-arrow" aria-hidden="true"></i>
                                 </span>
-                            </div>
-                            <div class="notify-form-outers">
-                                <div class="form-inner-sections">
-                                    <div class="each-rows-inputs">
-                                        <input type="text" placeholder="Enter your name">
-                                        <span class="users-pp">
-                                            <i class="fa fa-user" aria-hidden="true"></i>
-
-                                        </span>
-                                    </div>
-                                    <div class="each-rows-inputs">
-                                        <input type="text" placeholder="Enter your phone">
-                                        <span class="users-pp">
-                                            <i class="fa fa-mobile" aria-hidden="true"></i>
-
-                                        </span>
-
-                                    </div>
-                                    <div class="each-rows-inputs">
-                                        <input type="email" placeholder="Enter your email">
-                                        <span class="users-pp">
-                                            <i class="fa fa-envelope" aria-hidden="true"></i>
-
-                                        </span>
-                                    </div>
-                                    <div class="each-rows-inputs each-rows-inputss">
-                                        <span class="deadline-text-zone">
-                                            Time Zone
-                                        </span>
-                                        <div class="selected-timezone">
-
-
-                                            <select class="setected-time-zone">
-                                                <option>1</option>
-                                                <option>2</option>
-                                                <option>3</option>
-                                                <option>4</option>
-                                                <option>5</option>
-                                            </select>
-                                            <span class="users-pp">
-                                                <i class="fa fa-clock-o" aria-hidden="true"></i>
-
-                                            </span>
-
-                                        </div>
-
-
-
-
-                                    </div>
-
-                                    <div class="each-rows-inputs each-rows-inputss">
-                                        <span class="deadline-text">
-                                            Expected Calling Date
-                                        </span>
-                                        <div class="each-rows-deadline-sec">
-                                            <div class="date-starts-left">
-                                                <input type="date" placeholder="Enter start date" />
-                                            </div>
-                                            <div class="date-starts-right">
-                                                <input type="date" placeholder="Enter last date" />
-                                            </div>
-                                        </div>
-
-
-                                    </div>
-                                    <div class="each-rows-inputs">
-                                        <span class="deadline-text">
-                                            Preferable time
-                                        </span>
-                                        <div class="each-rows-deadline-time-zone">
-                                            <div class="date-starts-time">
-                                                <input type="time" placeholder="Enter start date">
-                                                <span class="users-pp">
-                                                    <i class="fa fa-clock-o" aria-hidden="true"></i>
+                             </div>
+                            
+                             <div class="notify-inner-sectionss">
+                                  <div class="lt-sec">
+                                       <div class="lt-inner-secs">
+                                          <div class="lt-headers">
+                                              <span class="hds">
+                                                Online Assignment Help
+                                              </span>
+                                                <span class="thumb-sc">
+                                                    <span class="thumb-iconss">
+                                                       <i class="fa fa-phone calls" aria-hidden="true"></i>
+                                                    </span>
 
                                                 </span>
+                                                <span class="sub-hds">
+                                                    24*7 Call & Support Services
+                                                </span>
+                                              
+
+                                          </div>
+                                          <div class="sub-text-notify">
+                                               <span class="sb-text">
+                                               Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                               Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                                               when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                               Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                              
+                                               </span>
+                                          </div>
+
+                                       </div>
+                                  </div>
+                                  <div class="rt-sec">
+                                        <div class="rt-inner-secs">
+                                            <div class="hurry-call-back">
+                                                  <div class="hurry-inner-sect">
+                                                       <span class="hurry">
+                                                         In a Hurry?
+                                                       </span>
+                                                       <span class="get-call">
+                                                          Get a callback
+                                                       </span>
+                                                  </div>
+                                                  <div class="hurry-inner-sc">
+                                                      <div class="get-each-sec">
+                                                           <div class="left-s-get">
+                                                                 <select class="selects-drop">
+                                                                       <option>IN (+91)</option>
+                                                                       <option>PAK(+93)</option>
+                                                                       <option>AUS(+91)</option>
+                                                                </select>
+                                                           </div>
+                                                           <div class="right-s-get">
+                                                                <input type="number" placeholder="Enter Phone Number"/>
+                                                           </div>
+                                                      </div>
+                                                      <div class="get-each-sec">
+                                                          <ul>
+                                                               <li> 
+                                                                   <div class="radio">
+                                                                        <input id="radio-1" name="radio" type="radio">
+                                                                        <label for="radio-1" class="radio-label">NEW ORDER</label>
+                                                                    </div>
+  
+                                                                </li>
+                                                                <li> 
+                                                                   <div class="radio">
+                                                                        <input id="radio-2" name="radio" type="radio">
+                                                                        <label for="radio-2" class="radio-label">EXISTING ORDER</label>
+                                                                    </div>
+  
+                                                                </li>
+                                                                <li> 
+                                                                   <div class="radio">
+                                                                        <input id="radio-3" name="radio" type="radio">
+                                                                        <label for="radio-3" class="radio-label">SELLS</label>
+                                                                    </div>
+  
+                                                                </li>
+                                                                <li> 
+                                                                   <div class="radio">
+                                                                        <input id="radio-4" name="radio" type="radio">
+                                                                        <label for="radio-4" class="radio-label">SERVICES</label>
+                                                                    </div>
+  
+                                                                </li>
+                                                           
+                                                          <ul>
+                                                      </div>
+                                                      <div class="get-each-sec">
+                                                         <span class="reason-for-callback">
+                                                         
+                                                           REASON FOR CALLBACK
+                                                         </span>
+                                                         <div class="text-reason">
+                                                             <textarea class="textareas" placeholder="Describe yourself here...">
+                                                             </textarea>
+                                                         </div>
+                                                      </div>
+                                                      <div class="get-each-sec">
+                                                         <span class="reason-for-callback">
+                                                             SUITABLE TIME FOR CONTACT
+                                                         </span>
+                                                          <ul>
+                                                               <li class="suitable-time"> 
+                                                                   <select class="selects-drop">
+                                                                        <option>HOURS</option>
+                                                                        <option>1</option>
+                                                                        <option>2</option>
+                                                                        <option>3</option>
+                                                                    </select>
+  
+                                                               </li>
+                                                               <li class="suitable-time"> 
+                                                                   <select class="selects-drop">
+                                                                        <option>MINUTES</option>
+                                                                        <option>1</option>
+                                                                        <option>2</option>
+                                                                        <option>3</option>
+                                                                    </select>
+  
+                                                               </li>
+                                                               <li class="suitable-time"> 
+                                                                   <select class="selects-drop">
+                                                                        <option>AM</option>
+                                                                        <option>PM</option>
+                                                                     
+                                                                    </select>
+  
+                                                               </li>
+                                                           
+                                                           
+                                                          <ul>
+                                                      </div>
+                                                  
+                                                  </div>
                                             </div>
-
                                         </div>
-
-
-                                    </div>
-                                </div>
-                                <div class="submit-buttin-sections">
-                                    <button type="submit" class="only-submit">
-                                        Submit
-
-                                    </button>
-
-                                </div>
-                            </div>
+                                   </div>
+                             </div>
+                          
 
 
                         </div>
