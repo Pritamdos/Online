c95d67797afbdc296845b237983baf0800a05344
sonipritam <pritam.kumar@venturepact.com>
2018-12-10 10:59:18 +0530

    changes done in feedback-icons

--- order.php
+++ order.php
@@ -5,20 +5,224 @@
     <title>Global Essay, Dissertation and Assignment Help </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta title="Looking for online essay, thesis or dissertation help? OnlineAssignmenthelp.com is your destination. We offer global assignment help service for all subjects at unbelievably cheap price.
-                ">
+    ">
     <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/custom.css">
     <link rel="stylesheet" type="text/css" href="./assets/css/font.css">
     <link rel="stylesheet" type="text/css" href="./assets/css/animated.css">
     <link rel="stylesheet" type="text/css" href="./assets/css/scrollanimations.css">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.css"
-    />
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://npmcdn.com/flickity@1.2/dist/flickity.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
 
 <body>
     <?php include './header.php'?>
+    <div class="onl-banner-sections">
+            <div class="onl-inrs-banner-sections-login-signups">
+                    <div class="order-details-sc">
+                        <div class="order-stepper-ir-sec">
+                            <div class="s-stepper">
+                                <div data-target="#stage1" id="stage-1-step" class="s-step active">
+                                    <span class="s-step-counter"></span>
+                                    <small>Basic information about job. Like Job Title</small>
+                                </div>
+                                <div data-target="#stage2" id="stage-2-step" class="s-step">
+                                    <span class="s-step-counter"></span>
+                                    <small>Information about required experience</small>
+                                </div>
+                               
+                            </div>
+                            <div class="s-stepper-content">
+                                    <div id="stage1" class="s-stepper-stage active">
+                                        <div class="stepper-form-inner-sec">
+                                            knfjkngkjgn
+                                        </div>
+                                        <div class="submit-button-sections-stepper">
+                                            <button onclick="hideStage(1);showStage(2)"  class="done-btn-sections">Next <i class="fa fa-right"></i></button>
+                                        </div>
+                                    </div>
+                                    <div id="stage2" class="s-stepper-stage">
+                                        <div class="stepper-form-inner-sec">
+                                            mfngnm
+                                        </div>
+                                          
+                                        <div class="submit-button-sections-stepper">
+                                            <button class="done-btn-sections">Done</button>
+                                        </div>
+                                    </div>
+                            </div>
+                        </div>
+                          
+                    </div>
+
+                    <div class="notifiicons">
+                        <div class="notif-flex">
+                            <div class="notif-btn">
+                                <span class="call-iconss swing">
+                                    <i class="fa fa-phone calls" aria-hidden="true"></i>
+                                </span>
+
+                                <span class="call-nows">
+                                    Call
+                                    <br> Back
+                                </span>
+
+                            </div>
+                        </div>
+                        <div class="notify-outer-poposs">
+                            <div class="notif-item notif-item-items animated slideInUp slideInDown hide">
+                                <div class="notify-inners-sec">
+                                    <div class="cross-ss">
+                                        <span class="closed-iconss" id="slide-down">
+                                        <i class="fa fa-times  drop-arrow" aria-hidden="true"></i>
+                                        </span>
+                                    </div>
+                                    
+                                    <div class="notify-inner-sectionss">
+                                        <div class="lt-sec">
+                                            <div class="lt-inner-secs">
+                                                <div class="lt-headers">
+                                                    <span class="hds">
+                                                        Online Assignment Help
+                                                    </span>
+                                                        <span class="thumb-sc">
+                                                            <span class="thumb-iconss animated zoomIn  swing">
+                                                            <i class="fa fa-phone calls" aria-hidden="true"></i>
+                                                            </span>
+
+                                                        </span>
+                                                        <span class="sub-hds">
+                                                            24*7 Call & Support Services
+                                                        </span>
+                                                    
+
+                                                </div>
+                                                <div class="sub-text-notify">
+                                                    <span class="sb-text">
+                                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                                                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                                    
+                                                    </span>
+                                                </div>
+
+                                            </div>
+                                        </div>
+                                        <div class="rt-sec">
+                                                <div class="rt-inner-secs">
+                                                    <div class="hurry-call-back">
+                                                        <div class="hurry-inner-sect">
+                                                            <span class="hurry">
+                                                                In a Hurry?
+                                                            </span>
+                                                            <span class="get-call">
+                                                                Get a callback
+                                                            </span>
+                                                        </div>
+                                                        <div class="hurry-inner-sc">
+                                                            <div class="get-each-sec">
+                                                                <div class="left-s-get">
+                                                                        <select class="selects-drop">
+                                                                            <option>IN (+91)</option>
+                                                                            <option>PAK(+93)</option>
+                                                                            <option>AUS(+91)</option>
+                                                                        </select>
+                                                                </div>
+                                                                <div class="right-s-get">
+                                                                        <input type="number" placeholder="Enter Phone Number"/>
+                                                                </div>
+                                                            </div>
+                                                            <div class="get-each-sec">
+                                                                <ul>
+                                                                    <li> 
+                                                                        <div class="radio">
+                                                                                <input id="radio-1" name="radio" type="radio">
+                                                                                <label for="radio-1" class="radio-label">NEW ORDER</label>
+                                                                            </div>
+        
+                                                                        </li>
+                                                                        <li> 
+                                                                        <div class="radio">
+                                                                                <input id="radio-2" name="radio" type="radio">
+                                                                                <label for="radio-2" class="radio-label">EXISTING ORDER</label>
+                                                                            </div>
+        
+                                                                        </li>
+                                                                
+                                                                <ul>
+                                                            </div>
+                                                            <div class="get-each-sec">
+                                                                <span class="reason-for-callback">
+                                                                
+                                                                REASON FOR CALLBACK
+                                                                </span>
+                                                                <div class="text-reason">
+                                                                    <textarea placeholder="Message" class="textareas" ></textarea>
+                                                                
+                                                                </div>
+                                                            </div>
+                                                            <div class="get-each-sec">
+                                                                <span class="reason-for-callback">
+                                                                    SUITABLE TIME FOR CONTACT
+                                                                </span>
+                                                                <ul>
+                                                                    <li class="suitable-time"> 
+                                                                        <select class="selects-drop">
+                                                                                <option>HOURS</option>
+                                                                                <option>1</option>
+                                                                                <option>2</option>
+                                                                                <option>3</option>
+                                                                            </select>
+        
+                                                                    </li>
+                                                                    <li class="suitable-time"> 
+                                                                        <select class="selects-drop">
+                                                                                <option>MINUTES</option>
+                                                                                <option>1</option>
+                                                                                <option>2</option>
+                                                                                <option>3</option>
+                                                                            </select>
+        
+                                                                    </li>
+                                                                    <li class="suitable-time"> 
+                                                                        <select class="selects-drop">
+                                                                                <option>AM</option>
+                                                                                <option>PM</option>
+                                                                            
+                                                                            </select>
+        
+                                                                    </li>
+                                                                
+                                                                
+                                                                <ul>
+                                                            </div>
+                                                        
+                                                        </div>
+                                                        <div class="call-back-submit">
+                                                                <button type="submit" class="only-submit">
+                                                                    Submit
+                                                                </button>
+
+                                                            </div>
+                                                    </div>
+                                                </div>
+                                        </div>
+                                    </div>
+                                
+
+
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+            </div>
+    </div>
+    
+    
     <?php include './footer.php'?>
 
 
@@ -29,7 +233,23 @@
 <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
 <script src="./assets/js/scrollanimations.js"></script>
 <script src="./assets/js/style.js"></script>
+<script>
+     function hideStage(stage)
+     {
 
+        $('#stage'+stage).removeClass('active');
+        $('#stage-'+stage+'-step').addClass('done');
+     }
 
+    function showStage(stage) 
+    {
+        $('#stage'+stage).addClass('active');
+        $('#stage-'+stage+'-step').addClass('active');
+    }
+
+    $(document).ready(function(){
+        
+    })
+</script>
 
 </html>
